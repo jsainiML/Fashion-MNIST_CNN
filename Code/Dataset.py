02e655dcbb4915f7a04927d14139bcab7f32c20d
jsainiML <135480841+jsainiML@users.noreply.github.com>
2023-07-12 01:29:07 -0400

    Update Dataset.py

--- Code/Dataset.py
+++ Code/Dataset.py
@@ -1,15 +1,15 @@
 # Loading Train/Test sets from torchvision datasets.
 train_data = datasets.FashionMNIST(root="data", # Which folder to download data to
                                    train=True,  # Do we want training data
-                                   download=True, # should we download true/yes false/no
+                                   download=True, # should we download yes/no
                                    transform= torchvision.transforms.ToTensor(), # to what format transform the data
                                    target_transform= None  # should we transform target label/data
                                    )
-test_data = datasets.FashionMNIST(root="data", # Which folder to download data to
-                                   train=False,  # Do we want training data / here we are extracting test data
-                                   download=True, # should we download true/yes false/no
-                                   transform= torchvision.transforms.ToTensor(), # to what format transform the data
-                                   target_transform= None  # should we transform target label/data
+test_data = datasets.FashionMNIST(root="data", 
+                                   train=False,  # Here we are extracting test data
+                                   download=True,
+                                   transform= torchvision.transforms.ToTensor(), 
+                                   target_transform= None 
                                    )
 
 
